fixed target variance; added save/load tree; added recalc of pL/var/delta from current tree; added tqdm total progress for nested loops

qsam/samplers/protocol_samplers.py
@@ -8,10 +8,10 @@ from ..circuit import partition, make_hash, unpack
 from ..fault_generators import Depolar
 from ..protocol import iterate
 from .sampler_mixins import SubsetAnalytics
-from .datatypes import CountNode, Variable, Constant, Fail, NoFail, Tree
+from .datatypes import CountNode, SampleTree
 
 import numpy as np
-from tqdm import tqdm
+from tqdm.notebook import tqdm
 
 # Cell
 ONE_QUBIT_GATES = {'H', 'X', 'Z'}
@@ -33,19 +33,21 @@ class Sampler:
     def run(self, n_samples, sample_range, err_params, RSE_target=1e-1, var=math.Wilson_var, eval_fns=None, verbose=False):
 
         # RSE: rel. standard error target
+        cnts = np.zeros(len(sample_range))
         fail_cnts = np.zeros(len(sample_range)) # one fail counter per sample point
         partitions = {circuit_hash: [partition(circuit, GATE_GROUPS[k]) for k in err_params.keys()]
                      for circuit_hash, circuit in self.protocol._circuits.items()}
 
-        for i,sample_pt in enumerate(sample_range): # n_samples at sample_pt
+        for i in tqdm(range(len(sample_range)), desc='Total'): # n_samples per sample_pt
 
-            p_phy = np.array(list(err_params.values())) * sample_pt
+            p_phy = np.array(list(err_params.values())) * sample_range[i]
 
-            for j in tqdm(range(n_samples)):
+            for _ in tqdm(range(n_samples), desc=f'p_phy={",".join(list(f"{p:.2E}" for p in p_phy))}', leave=True):
 
                 sim = self.simulator(self.n_qubits)
                 p_it = iterate(self.protocol, eval_fns)
                 node = next(p_it)
+                cnts[i] += 1
 
                 if verbose: print(f'--- Protocol run {j} ---')
 
@@ -71,15 +73,14 @@ class Sampler:
                         if _node == 'COR': print(f"Node {_node}, Circuit {circuit} -> {node}")
                         else: print(f"Node {_node}, Faults {pauli_faults}, Measured {msmt}-> {node}")
 
-                # if j > 1:
-                #     p_L_j = fail_cnts[i] / j
-                #     # print(fail_cnts[i], j)
-                #     if p_L_j != 0 and np.sqrt( var(p_L_j, j) )/ p_L_j < RSE_target:
-                #         # print(f'RSE target of {RSE_target} reached.')
-                #         break
+                p_L = fail_cnts[i] / cnts[i]
+                if p_L != 0 and np.sqrt( var(p_L, cnts[i]) ) / p_L < RSE_target:
+                    print(f'RSE target of {RSE_target} reached.')
+                    break
 
-        p_L = fail_cnts / n_samples
-        std = np.sqrt( var(p_L, n_samples) )
+        p_L = fail_cnts / cnts
+        std = np.sqrt( var(p_L, cnts) )
+
         return p_L, std
 
 # Cell
@@ -91,10 +92,9 @@ class SubsetSampler(SubsetAnalytics):
         self.protocol = protocol
         self.simulator = simulator
         self.n_qubits = len(set(q for c in protocol._circuits.values() for q in unpack(c)))
-        self.tree = tree if tree else Tree()
+        self.tree = SampleTree()
 
-    def run(self, n_samples, sample_range, err_params, chi_min=1e-2, p_max=0.1, RSE_target=1e-1, ERV_sel=True, var=math.Wilson_var, eval_fns={}):
-
+    def setup(self, sample_range, err_params):
         p_phy_per_partition = np.array([[p_phy * mul for p_phy in sample_range] for mul in err_params.values()]).T
         partitions = {circuit_hash: [partition(circuit, GATE_GROUPS[k]) for k in err_params.keys()]
                       for circuit_hash, circuit in self.protocol._circuits.items() if circuit._noisy}
@@ -102,61 +102,75 @@ class SubsetSampler(SubsetAnalytics):
                   for circuit_hash, pars in partitions.items()}
         Aws = {circuit_hash: SubsetSampler.subset_occurence(partitions[circuit_hash], w_vecs, p_phy_per_partition)
                for circuit_hash, w_vecs in w_vecs.items()}
+        return partitions, w_vecs, Aws
 
+    def rate(self, sample_range, err_params):
+        partitions, w_vecs, Aws = self.setup(sample_range, err_params)
+        return self.tree.rate(Aws)
+
+    def var(self, sample_range, err_params):
+        partitions, w_vecs, Aws = self.setup(sample_range, err_params)
+        return self.tree.var(Aws)
+
+    def delta(self, sample_range, err_params):
+        partitions, w_vecs, Aws = self.setup(sample_range, err_params)
+        return self.tree.delta(Aws)
+
+    def run(self, n_samples, sample_range, err_params, chi_min=1e-2, p_max=0.1, RSE_target=1e-1, ERV_sel=True, eval_fns={}):
+
+        partitions, w_vecs, Aws = self.setup(sample_range, err_params)
         Aws_at_p_max = {circuit_hash: SubsetSampler.subset_occurence(partitions[circuit_hash], w_vecs, np.array(p_max))
                         for circuit_hash, w_vecs in w_vecs.items()}
 
-        for _ in tqdm(range(n_samples)):
+        for _ in tqdm(range(n_samples), desc='Total'):
 
             chi = 1
             sim = self.simulator(self.n_qubits)
             p_it = iterate(self.protocol, eval_fns)
             node = next(p_it)
-            tree_node = self.tree.add(name=node, node_cls=CountNode)
+            tree_node = self.tree.add(name=node)
             tree_node.counts += 1
 
             while node:
 
                 if not self.protocol.out_edges(node):
+                    tree_node.is_fail = True
                     break # terminal node reached
 
                 circuit_hash, circuit = self.protocol.circuit_from_node(node)
 
                 if circuit_hash not in partitions.keys(): # handle circuits created at runtime
                     msmt = sim.run(circuit)
-                    tree_node = self.tree.add(name=(0,),
-                                              node_cls=Constant,
-                                              parent=tree_node,
-                                              data={'Aw': np.ones(shape=p_phy_per_partition.shape[0]),
-                                                    'Aw_p_max': np.array([1])})
+                    Aws[circuit_hash] = np.ones(shape=len(err_params))
+                    Aws_at_p_max[circuit_hash] = np.array([1])
+                    tree_node = self.tree.add(name=(0,), parent=tree_node, ckey=(circuit_hash, 0),
+                                              is_deterministic=circuit._ff_deterministic)
                 else:
                     ids = np.where(chi * Aws_at_p_max[circuit_hash] > chi_min)[0]
                     if len(ids) == 0 or not circuit._noisy:
                         idx = 0
                     else:
-                        delta = self.tree.delta(ckey='Aw_p_max')
-                        v_L = self.tree.var(ckey='Aw_p_max')
-                        p_L = self.tree.rate(ckey='Aw_p_max')
+                        delta = self.tree.delta(Aws_at_p_max)
+                        v_L = self.tree.var(Aws_at_p_max)
+                        p_L = self.tree.rate(Aws_at_p_max)
 
                         if not ERV_sel:
                             idx = np.random.choice(ids)
                         else:
                             erv_deltas = []
                             for idx in ids:
-                                Aw = Aws[circuit_hash][idx]
-                                Aw_p_max = Aws_at_p_max[circuit_hash][idx]
                                 w_vec = w_vecs[circuit_hash][idx]
 
-                                _tree_node = self.tree.add(w_vec, node_cls=Constant, parent=tree_node, data={'Aw': Aw, 'Aw_p_max': Aw_p_max})
-                                __tree_node = self.tree.add('FAIL', node_cls=Fail, parent=_tree_node)
-                                _delta = self.tree.delta(ckey='Aw_p_max')
+                                _tree_node = self.tree.add(w_vec, parent=tree_node, ckey=(circuit_hash, idx))
+                                __tree_node = self.tree.add('FAIL', parent=_tree_node, is_fail=True)
+                                _delta = self.tree.delta(Aws_at_p_max)
                                 _rate = __tree_node.rate
 
                                 _tree_node.counts += 1
-                                v_L_minus = self.tree.var(ckey='Aw_p_max')
+                                v_L_minus = self.tree.var(Aws_at_p_max)
 
                                 __tree_node.counts += 1
-                                v_L_plus = self.tree.var(ckey='Aw_p_max')
+                                v_L_plus = self.tree.var(Aws_at_p_max)
 
                                 _v_L = _rate * v_L_plus + (1 - _rate) * v_L_minus
                                 erv_delta = np.abs(v_L - _v_L) + (delta - _delta)
@@ -180,27 +194,23 @@ class SubsetSampler(SubsetAnalytics):
                     fault_circuit = Depolar.gen_circuit(len(circuit), faults)
                     msmt = sim.run(circuit, fault_circuit)
 
-                    tree_node = self.tree.add(name=w_vec, node_cls=Constant, parent=tree_node, data={'Aw': Aw, 'Aw_p_max': Aw_p_max},
-                                              deterministic=True if circuit._ff_deterministic and not any(w_vec) else False)
+                    tree_node = self.tree.add(name=w_vec, parent=tree_node, ckey=(circuit_hash, idx),
+                                              is_deterministic=True if circuit._ff_deterministic and not any(w_vec) else False)
 
-                tree_node.counts += 1
                 node = p_it.send(msmt if msmt==None else int(msmt,2)) # exchange with iterator
-
-                if node == None: name, node_cls = "NO FAIL", NoFail
-                elif node == 'FAIL': name, node_cls = node, Fail
-                else: name, node_cls = node, Variable
-                tree_node = self.tree.add(name=name, node_cls=node_cls, parent=tree_node)
+                if node == None: name = "NO FAIL"
+                else: name = node
+                tree_node.counts += 1
+                tree_node = self.tree.add(name=name, parent=tree_node)
                 tree_node.counts += 1
 
+            if p_L > 0 and (np.sqrt(v_L) + delta) / p_L < RSE_target:
+                print(f'RSE target of {RSE_target} reached.')
+                break
 
-#             if p_L > 0 and (np.sqrt(v_L) + delta) / p_L < RSE_target:
-#                 print(f'RSE target of {RSE_target} reached.')
-#                 break
-
-        v_L = self.tree.var(ckey='Aw')
-        p_L_low = self.tree.rate(ckey='Aw')
-        p_L_low = np.zeros_like(v_L) if type(p_L_low) == int else p_L_low # if all paths fault free: return array of 0s
-        p_L_up = p_L_low + self.tree.delta(ckey='Aw')
+        v_L = self.tree.var(Aws)
+        p_L_low = self.tree.rate(Aws)
+        p_L_up = p_L_low + self.tree.delta(Aws)
 
         return p_L_low, p_L_up, np.sqrt(v_L)
 