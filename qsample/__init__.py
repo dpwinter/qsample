7a633282574eb7593684ac0c8aefa890af79ed95
Don Winter <don@wintr.de>
2022-12-05 11:20:43 +0100

    Changed readme

--- qsample/__init__.py
+++ qsample/__init__.py
@@ -1,12 +1,1 @@
 __version__ = "0.0.1"
-
-from .circuit import Circuit, unpack, GATES
-from .protocol import Protocol
-from .noise import ErrorModel, E0, E1, E2, E3
-from .callbacks import Callback, PlotStats, RelStdTarget, StatsPerSample, VerboseCircuitExec
-
-from .sim.stabilizer import StabilizerSimulator
-from .sim.statevector import StatevectorSimulator
-from .sampler.direct import DirectSampler
-from .sampler.subset import SubsetSampler
-from .sampler.base import Sampler
